af54cfc3c0eb5febf0cca5b681fccc0977f91303
FerSoriano <fer8f10@hotmail.com>
2025-10-12 21:51:16 -0600

    nueva logica en main para controlar la ejecucion del programa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,68 @@
-import java.time.LocalDateTime;
+
+import utils.Menu;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static Menu menu = new Menu();
+
     public static void main(String[] args) {
-        Paciente paciente = new Paciente("Pepe", "3355118622", 'M');
+        menu.mostrarTitulo();
 
-        Cita cita = new Cita(paciente, LocalDateTime.now().plusHours(3), "Cita medica", 30);
+        while (true) {
+            menu.menuPrincipal();
+            int opcion = scanner.nextInt();
+            if (opcion < 1 || opcion > 4) {
+                menu.mostrarMensajeConsola("Opcion incorrecta. Intenta de nuevo");
+                continue;
+            }
+            if (opcion == 1) irAMenuPaciente();
+            if (opcion == 2) menu.mostrarMensajeConsola("Creando nueva cita");
+            if (opcion == 3) menu.mostrarMensajeConsola("Viendo agenda");
+            if (opcion == 4) {
+                menu.mostrarMensajeConsola("Saliendo del sistema");
+                break;
+            }
+        }
 
-        if(cita.proximaCita() != null) {
-            System.out.println("Proxima cita: " + cita.proximaCita());
-        } else {
-            System.out.println("No hay citas registradas.");
+    }
+
+    public static void irAMenuPaciente() {
+        while (true) {
+            menu.menuPacientes();
+            int opcion = scanner.nextInt();
+            if (opcion < 1 || opcion > 4) {
+                menu.mostrarMensajeConsola("Opcion incorrecta. Intenta de nuevo");
+                continue;
+            }
+            if (opcion == 1) crearPaciente();
+            if (opcion == 2) Paciente.mostrarPacientes();
+            if (opcion == 3) menu.mostrarMensajeConsola("Implementar eliminar paciente"); // TODO: Implementar eliminar paciente.
+            if (opcion == 4) break;
+        }
+    }
+
+    public static void crearPaciente() {
+        scanner.nextLine(); // se limpia el buffer
+        menu.mostrarMensajeConsola("CREAR PACIENTE", 20);
+        System.out.print("\nNombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Telefono (10 digitos): ");
+        String telefono = scanner.nextLine();
+        System.out.print("Genero (M/F): ");
+        String generoString = scanner.next();
+        char genero = generoString.charAt(0); // Obtener el primer caracter
+
+        try {
+            Paciente paciente = Paciente.crearPaciente(nombre, telefono, genero);
+            if (paciente == null) {
+                menu.mostrarMensajeConsola("Datos incorrectos del paciente. Intenta de nuevo.");
+            } else {
+                menu.mostrarMensajeConsola("Paciente " + paciente.getNombre() + " creado exitosamente.");
+            }
+        } catch (IllegalArgumentException e) {
+            menu.mostrarMensajeConsola("Error: " + e.getMessage());
         }
     }
 }
